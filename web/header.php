f6d01740c797f280d3a7a2001993252c1c8ca38d
mikael <mikael.bobinet@g-mail.fr>
2018-12-06 23:25:27 +0100

    Ajout du dossier web!

---
+++ web/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="./header.css" media="all"/> 
+</head>
+<body>
+
+<div class="header">
+  <a href="#default" class="logo">CompanyLogo</a>
+  <div class="header-right">
+    <a class="active" href="squelette.php">Home</a>
+    <a href="onglet2.php">Onglet2</a>
+    <a href="onglet3.php">Onglet3</a>
+  </div>
+</div>
+</body>
+</html>
---
+++ web/onglet2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="./tout.css" media="all"/> 
+
+<link rel="stylesheet" type="text/css" href="./header.css" media="all"/> 
+</head>
+<body>
+
+<div class="header">
+  <a href="#default" class="logo">CompanyLogo</a>
+  <div class="header-right">
+    <a href="squelette.php">Home</a>
+    <a class="active" href="onglet2.php">Onglet2</a>
+    <a href="onglet3.php">Onglet3</a>
+  </div>
+</div>
+</body>
+
+
+</head>
+<body>
+
+<div class="header2">
+  <h1>histoire des biscottes</h1>
+</div>
+
+<div class="row">
+  <div class="col-3 menu">
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3456789</li>
+      <li>10 11 12 13 14 15 16 17 18 19</li>
+    </ul>
+  </div>
+
+  <div class="col-6">
+    <h1>Pistache</h1>
+    <p>La pistache se déguste souvent à l'apéro.</p>
+  </div>
+
+  <div class="col-3 right">
+    <div class="aside">
+      <h2>What?</h2>
+      <p>graine</p>
+      <h2>Where?</h2>
+      <p>racine</p>
+      <h2>How?</h2>
+      <p>la puissance de la vie</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+</div>
+
+</body>
+</html>
