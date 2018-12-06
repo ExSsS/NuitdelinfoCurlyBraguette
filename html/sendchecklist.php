52b8169874164a7d849f8cbc4d14e4ffc76ed7e6
picachoc <loic.lachiver@gmail.com>
2018-12-07 00:28:13 +0100

    mal écrit

---
+++ html/sendchecklist.php
@@ -0,0 +1,16 @@
+<?php
+
+echo $_POST['checkboxpipi']; 
+
+/*echo "ouiouioui"
+$checklist->pipi = $_POST['checkboxpipi']; 
+$checklist->glouglou = $_POST['checkboxglouglou']; 
+
+$myJSON = json_encode($checklist);
+
+$filename = "/json/oui.json"
+file_put_contents ( string $filename , mixed (echo $myJSON) [, int $flags = 0 [, resource $context ]] )
+
+echo "OUIOUIOUIOUIOUI"*/
+
+?>
