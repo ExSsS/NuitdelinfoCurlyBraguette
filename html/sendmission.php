303f5d77ce26f9f1e8b9b19c01ad5c6e16047f46
picachoc <loic.lachiver@gmail.com>
2018-12-07 05:51:32 +0100

    oui

--- html/sendmission.php
+++ html/sendmission.php
@@ -6,7 +6,7 @@ $path="/data/missions.txt";
 $str = 'date : '.$_POST['date'].', duree : '.$_POST['duree'].', equipement : '.$_POST['equipement'];
 $current = file_get_contents($path);
 echo $current;
-$current .= $str;
+$current = $current.$str;
 echo $current;
 file_put_contents($path,$current);
 
