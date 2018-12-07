fcff8b7d4f1b98a98af1c7159ae7db54d7c58d05
picachoc <loic.lachiver@gmail.com>
2018-12-07 03:50:05 +0100

    oui

--- html/sendchecklist.php
+++ html/sendchecklist.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html>
 <?php
-$checklist->pipi = $_POST['chk_panneauSolaires'];
-
+$checklist->date = 
 $checklist->panneauSolaires = $_POST['chk_panneauSolaires'];
 $checklist->eoliennes = $_POST['chk_eoliennes'];
 $checklist->robots = $_POST['chk_robots'];
 $checklist->systemesSurvie = $_POST['chk_systemesSurvie'];
 $checklist->camp = $_POST['chk_camp'];
 $checklist->sante = $_POST['chk_sante'];
+$checklist_tot->date('l jS \of F Y')=json_encode($checklist);
 
-$myjson = json_encode($checklist);
+$chk_json = json_encode($checklist_tot);
+echo $checklist_tot;
 
-$fp = fopen('data/checklist_'.date.'json', 'w');
-fwrite($fp, $myjson);
+if (stristr($data, 'certain word')) {
+    return "replaement line!\n";
+  }
+  return $data;
+
+$fp = fopen('data/checklistjson', 'w');
+fwrite($fp, $chk_json);
 fclose($fp);
 ?>
 </html>
