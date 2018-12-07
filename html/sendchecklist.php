f93b8a7d0b3db7c2b46f8ce53e6b9f609155c50a
picachoc <loic.lachiver@gmail.com>
2018-12-07 05:17:43 +0100

    oui

--- html/sendchecklist.php
+++ html/sendchecklist.php
@@ -4,7 +4,7 @@
 $path="/data/checklist.json";
 echo "REPONDS MOI FDP";
 $date = date('l jS \of F Y');
-$checklist->date=$date;/*
+$checklist->date=$date;
 $checklist->panneauSolaires = $_POST['chk_panneauSolaires'];
 $checklist->eoliennes = $_POST['chk_eoliennes'];
 $checklist->robots = $_POST['chk_robots'];
@@ -12,7 +12,7 @@ $checklist->systemesSurvie = $_POST['chk_systemesSurvie'];
 $checklist->camp = $_POST['chk_camp'];
 $checklist->sante = $_POST['chk_sante'];
 
-echo $checklist;
+echo $checklist;/*
 $json_decode=json_decode(file_get_contents($path),true);
 echo file_get_contents($path);
 echo $json_decode;/*
