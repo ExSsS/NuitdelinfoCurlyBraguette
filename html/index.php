92749ae30c62440382317c2f8831a4b7836d0100
picachoc <loic.lachiver@gmail.com>
2018-12-07 01:02:08 +0100

    oui

--- html/index.php
+++ html/index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
+
+<?php
+echo "oui";
+?>
+
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="./tout.css" media="all"/> 
