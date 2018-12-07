78c9ca3f57d125ca7394caa8e19920a6b84e29c1
mikael <mikael.bobinet@g-mail.fr>
2018-12-07 02:40:45 +0100

    tout.ccs

--- html/index.php
+++ html/index.php
@@ -44,7 +44,7 @@ ws
     <div class="meteo">
     <iframe src="https://www.meteoblue.com/en/weather/widget/three/talence_france_2973495?geoloc=fixed&nocurrent=0&noforecast=0&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=monochrome"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 460px;height: 595px">
     </iframe><div><!-- DO NOT REMOVE THIS LINK -->
-      <a href="https://www.meteoblue.com/en/weather/forecast/week/talence_france_2973495?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget" target="_blank">meteoblue</a> </div>
+      <a href="https://www.meteoblue.com/en/weather/forecast/week/talence_france_2973495?utm_source=weather_widget&utm_medium=linkus&utm_content=three&utm_campaign=Weather%2BWidget" target="_blank" class="petitBidule">meteoblue</a> </div>
     </div>
   </div>
 
