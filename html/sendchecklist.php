0529f799a7fb654914fd508c426809190bb7ab49
picachoc <loic.lachiver@gmail.com>
2018-12-07 01:49:22 +0100

    oui

--- html/sendchecklist.php
+++ html/sendchecklist.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html>
 <?php
-$checklist->pipi = $_POST['checkboxpipi'];
-$checklist->glouglou = $_POST['checkboxglouglou'];
+$checklist->pipi = $_POST['chk_panneauSolaires'];
+
+$checklist->panneauSolaires = $_POST['chk_panneauSolaires'];
+$checklist->eoliennes = $_POST['chk_eoliennes'];
+$checklist->robots = $_POST['chk_robots'];
+$checklist->systemesSurvie = $_POST['chk_systemesSurvie'];
+$checklist->camp = $_POST['chk_camp'];
+$checklist->sante = $_POST['chk_sante'];
 
 $myjson = json_encode($checklist);
 echo $myjson;
+
+$filename = "/data/checklist.json";
+file_put_contents ( string $filename , mixed $myjson [, int $flags = 0 [, resource $context ]] );
+
 ?>
 </html>
