e9e86da96aff13d9ea8eb2a6f1d5ba233d2cc525
mikael <mikael.bobinet@g-mail.fr>
2018-12-07 05:45:23 +0100

    Test ajout API map

--- html/index.php
+++ html/index.php
@@ -80,12 +80,8 @@
     </div>
 </div>
     <div class="aside">
-    <iframe
-  width = "600"
-  hauteur = "450"
-  frameborder = "0" style = "border: 0"
-  src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyCLG4EHtcL496f0pu41J-mosN6V6snqWuI"Autoriser l’écran>>
-</ iframe>
+    <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=namib&key=AIzaSyCLG4EHtcL496f0pu41J-mosN6V6snqWuI" allowfullscreen>
+    </iframe>
     </div>
   </div>
 </div>
